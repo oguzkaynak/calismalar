d75c5700628da3f6cff7856ad4c285aa050fd8fe
Oğuz Kaynak <oguz-kaynak@hotmail.com>
2018-04-20 15:05:57 +0300

    Çalışma dosyası eklendi

---
+++ 200418-01.rb
@@ -0,0 +1,26 @@
+=begin
+Veri Türleri
+- Numeric
+  - Integer
+  - Float
+  - Rational
+  - Complex
+- String
+  - Tek tırnak
+  - Çift tırnak
+  - Metin içine gömme(embedding)
+- Boolean
+  - false (false veya nil)
+  - true (geri kalan herşey true)
+  - nil
+- Symbol
+- Compound Data Types
+  - Array (Dizi)
+  - Hash (Sözlük)
+- Object Oriented Programming
+  - Class
+  - Object
+  - Inheritance
+  - Mixin
+  - Module
+  =end
